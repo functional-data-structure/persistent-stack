587058d1a476721b979fd97ab8c2b49d41782f1a
Aurélien Ooms <aurelien.ooms@gmail.com>
2020-09-06 18:24:12 +0200

    :sparkles: feat: Export default implementation functions.

--- src/index.js
+++ src/index.js
@@ -5,4 +5,17 @@ import withoutMethods from './withoutMethods';
 /* eslint import/no-anonymous-default-export: [2, {"allowObject": true}] */
 export default withoutMethods;
 
-export {DONE_ITERATOR, withMethods, withoutMethods};
+const {empty, from, isEmpty, push, peek, pop, iter} = withoutMethods;
+
+export {
+	DONE_ITERATOR,
+	withMethods,
+	withoutMethods,
+	empty,
+	from,
+	isEmpty,
+	push,
+	peek,
+	pop,
+	iter,
+};
---
+++ test/src/exports.js
@@ -0,0 +1,25 @@
+import test from 'ava';
+
+import {empty, from, isEmpty, push, pop, peek, iter} from '../../src';
+
+test('can import empty', (t) => {
+	t.true(empty !== undefined);
+});
+test('can import from', (t) => {
+	t.true(from !== undefined);
+});
+test('can import isEmpty', (t) => {
+	t.true(isEmpty !== undefined);
+});
+test('can import push', (t) => {
+	t.true(push !== undefined);
+});
+test('can import pop', (t) => {
+	t.true(pop !== undefined);
+});
+test('can import peek', (t) => {
+	t.true(peek !== undefined);
+});
+test('can import iter', (t) => {
+	t.true(iter !== undefined);
+});
