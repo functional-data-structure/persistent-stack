c5706f6d218db8f1fe121fb1deed8b51787c86d3
make-github-pseudonymous-again <5165674+make-github-pseudonymous-again@users.noreply.github.com>
2021-07-30 16:34:48 +0200

    :boom: refactor!: Remove default export.
    BREAKING CHANGE: The default export is gone.

--- src/index.js
+++ src/index.js
@@ -2,9 +2,6 @@ import DONE_ITERATOR from './DONE_ITERATOR.js';
 import withMethods from './withMethods.js';
 import withoutMethods from './withoutMethods.js';
 
-/* eslint import/no-anonymous-default-export: [2, {"allowObject": true}] */
-export default withoutMethods;
-
 const {empty, from, isEmpty, push, peek, pop, iter} = withoutMethods;
 
 export {
